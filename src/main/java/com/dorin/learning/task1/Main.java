8b0fef889cdd5222d6f1caec8354d9e190396a15
Dorin <dorin.popa1995@gmail.com>
2017-09-19 14:05:50 +0300

    Implement task using stream API

--- src/main/java/com/dorin/learning/task1/Main.java
+++ src/main/java/com/dorin/learning/task1/Main.java
@@ -1,7 +1,31 @@
 package com.dorin.learning.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        // Create a function that receives a list of numbers, and computes the average for just the positive ones
+        List<Number> numbers = new ArrayList<>();
+        numbers.add(-5);
+        numbers.add(8);
+        numbers.add(7);
+        numbers.add(-21);
 
+        Number average = computeAverage(numbers);
+        System.out.println(average);
+    }
+
+    private static Number computeAverage(List<Number> numbers) {
+        List<Number> positiveOnes = numbers.stream()
+                .filter(element -> (int)element > 0)
+                .collect(Collectors.toList());
+
+        Number sum = positiveOnes.stream()
+                .reduce((first, second) -> (int)first + (int)second)
+                .get();
+
+        return (double)sum.intValue() / positiveOnes.size();
     }
 }
