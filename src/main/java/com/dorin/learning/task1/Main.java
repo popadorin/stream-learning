8a8109a892d8dd7b6ad8e68689ef7327eeec6aa2
Dorin <dorin.popa1995@gmail.com>
2017-09-19 16:21:03 +0300

    Refactor method by squashing two streams computing in one

--- src/main/java/com/dorin/learning/task1/Main.java
+++ src/main/java/com/dorin/learning/task1/Main.java
@@ -2,6 +2,7 @@ package com.dorin.learning.task1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,19 +12,22 @@ public class Main {
         numbers.add(8);
         numbers.add(7);
         numbers.add(-21);
+        numbers.add(15);
 
         Number average = computeAverage(numbers);
-        System.out.println(average);
+        if (average.equals(0)) {
+            System.out.println("Some problems in the computeAverage method");
+        } else {
+            System.out.println(average);
+        }
     }
 
     private static Number computeAverage(List<Number> numbers) {
-        Number sum = numbers.stream()
-                .filter(element -> (int)element > 0)
-                .reduce((first, second) -> (int)first + (int)second).get();
+        OptionalDouble average = numbers.stream()
+                .filter(number -> (int)number > 0)
+                .mapToInt(a -> (int)a)
+                .average();
 
-        long numberOfPositiveNumbers = numbers.stream()
-                .filter(number -> (int)number > 0).count();
-
-        return (double)sum.intValue() / numberOfPositiveNumbers;
+        return average.isPresent() ? average.getAsDouble() : 0;
     }
 }
