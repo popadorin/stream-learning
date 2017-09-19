c51285475f21fd0275f07dccaaf3196fc3556a59
Dorin <dorin.popa1995@gmail.com>
2017-09-19 18:16:01 +0300

    Implement subtask3 without stream
    Some issues with the condition of the subtask3

--- src/main/java/com/dorin/learning/task2/Course.java
+++ src/main/java/com/dorin/learning/task2/Course.java
@@ -4,4 +4,19 @@ public class Course {
     private String title;
     private Integer duration;
     private Double score;
+
+    public Course(String title, Integer duration, Double score) {
+        this.title = title;
+        this.duration = duration;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "title='" + title + '\'' +
+                ", duration=" + duration +
+                ", score=" + score +
+                '}';
+    }
 }
--- src/main/java/com/dorin/learning/task2/Main.java
+++ src/main/java/com/dorin/learning/task2/Main.java
@@ -1,23 +1,45 @@
 package com.dorin.learning.task2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a function that receives a list of Students, and returns the list of their names
+        // 1. Create a function that receives a list of Students, and returns the list of their names
+        // 2. Create a function that finds the maximum age form a list of students
+        // 3. Create a function that prints out all the courses taken by a list of students, without duplicates
+        // 4. Create a function that computes the average score for each student in a list
+        // 5. Create a function that returns a Map of: student name <-> list of course titles, from a list of students
+        // 6. Filter out the students that have taken less than 2 courses, and sort them by the number of courses taken
+
+        List<Course> courses1 = new ArrayList<>();
+        courses1.add(new Course("Maths", 120, 8.0));
+        courses1.add(new Course("Chemistry", 120, 8.0));
+        courses1.add(new Course("Informatics", 120, 6.0));
+
+        List<Course> courses2 = new ArrayList<>();
+        courses2.add(new Course("Maths", 120, 10.0));
+        courses2.add(new Course("Geography", 120, 8.0));
+        courses2.add(new Course("Physical education", 120, 7.0));
+
+        List<Course> courses3 = new ArrayList<>();
+        courses3.add(new Course("Maths", 120, 10.0));
+        courses3.add(new Course("Psychology", 120, 8.0));
+        courses3.add(new Course("Geography", 120, 9.0));
+
         List<Student> students = new ArrayList<>();
-        students.add(new Student("Jora", 21, null));
-        students.add(new Student("Grisha", 22, null));
-        students.add(new Student("Anton", 20, null));
+        students.add(new Student("Jora", 21, courses1));
+        students.add(new Student("Grisha", 22, courses2));
+        students.add(new Student("Anton", 20, courses3));
 
         List<String> names = studentNames(students);
         names.forEach(System.out::println);
 
         OptionalInt getMaximumAge = getMaximumAge(students);
         System.out.println(getMaximumAge);
+
+        List<Course> courses = getCourses(students);
+        courses.forEach(System.out::println);
     }
 
     private static List<String> studentNames(List<Student> students) {
@@ -28,5 +50,20 @@ public class Main {
 
     private static OptionalInt getMaximumAge(List<Student> students) {
         return students.stream().mapToInt(Student::getAge).max();
+    }
+
+    private static List<Course> getCourses(List<Student> students) {
+        List<Course> courses = new ArrayList<>();
+        for (Student student : students) {
+            List<Course> coursesTemp = student.getCourses();
+            for (Course course : coursesTemp) {
+                courses.add(course);
+            }
+        }
+
+        Set<Course> uniqueCourses = new HashSet<>(courses);
+        List<Course> ll = new ArrayList<>(uniqueCourses);
+
+        return ll;
     }
 }
